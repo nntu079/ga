9e45d7b6e4ea549a1a1f8fa805f882b11beb6efb
Tu <nntu079>
2023-05-11 06:02:39 +0700

    remove old crossover function

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 import utils
 import copy
 
-
 def mutation(individual,capacity,suppliers,current_capacity):
     #return new individual (deep copy)
     individual = copy.deepcopy(individual) 
@@ -31,48 +30,36 @@ def mutation(individual,capacity,suppliers,current_capacity):
 
     return [False, individual]
 
-def chunkIt(seq, num):
-    avg = len(seq) / float(num)
-    out = []
-    last = 0.0
+def crossover(individual1, individual2):
+    index1 = utils.getRamdomIndex(individual1)
+    index2 = utils.getRamdomIndex(individual2)
+    print(index1)
+    print(index2)
 
-    while last < len(seq):
-        out.append(seq[int(last):int(last + avg)])
-        last += avg
+    bit1 = individual1[index1[0]][index1[1]]
+    bit2 = individual2[index2[0]][index2[1]]
 
-    return out
-
-def flatten(arr):
-
-    return [j for sub in arr for j in sub]
-
-def crossover1(individual1,individual2):
-    new_individual1 = copy.deepcopy(individual1)
-    new_individual2 = copy.deepcopy(individual2)
-
-    new_part_individual1 = chunkIt(new_individual2,2)[1]
-    old_part_individual1_flat = flatten(chunkIt(new_individual1,2)[1])
-    new_part_individual2 = chunkIt(new_individual1,2)[1]
-    old_part_individual2_flat = flatten(chunkIt(new_individual2,2)[1])
-
-    count = 0
-    for index1,value1 in enumerate(new_part_individual1):
-        for index2,value2 in enumerate(value1):
-            if(count < len(old_part_individual1_flat)):
-                new_part_individual1[index1][index2] = old_part_individual1_flat[count]
-            else:
-                del new_part_individual1[index1][index2]
-            count = count + 1
-
-    while(count < len(old_part_individual1_flat)):
-        new_part_individual1.append([old_part_individual1_flat[count]])
-        count= count + 1
+    newIndividual1 = copy.deepcopy(individual1)
+    newIndividual2 = copy.deepcopy(individual2)
     
-    for index1,value1 in enumerate(new_part_individual1):
-        if(len(value1)==0):
-            del new_part_individual1[index1] 
+    for i1 in range(0, len(individual1)):
+        for i2 in range(0, len(individual1[i1])):
+            if (individual1[i1][i2] == bit1):
+                newIndividual1[i1][i2] = bit2
+            elif (individual1[i1][i2] == bit2):
+                newIndividual1[i1][i2] = bit1
+
+    for i1 in range(0, len(individual2)):
+        for i2 in range(0, len(individual2[i1])):
+            if (individual2[i1][i2] == bit1):
+                newIndividual2[i1][i2] = bit2
+            elif (individual2[i1][i2] == bit2):
+                newIndividual2[i1][i2] = bit1
+
+    if(newIndividual1 == individual1):
+        newIndividual1 = []
+    if(newIndividual2 == individual2):
+        newIndividual2 = []
+
+    return [newIndividual1,newIndividual2]\
     
-    return chunkIt(new_individual1,2)[0] + new_part_individual1
-    
-def crossover(indv1,indv2):
-    return [crossover1(indv1,indv2),crossover1(indv2,indv1)]
--- one-file.py
+++ one-file.py
@@ -1,10 +1,10 @@
 import random
 import copy
 import pandas as pd
-import csv
 import numpy as np
 import itertools
 
+############### UTILS #############
 
 def sum_gen(gen, suppliers):
     sum = 0
@@ -252,3 +252,212 @@ def write_output(output,file_path):
                 f.write('\n')
 
             f.write('\n')
+
+########### functions ########
+
+def mutation(individual,capacity,suppliers,current_capacity):
+    #return new individual (deep copy)
+    individual = copy.deepcopy(individual) 
+
+    #print(individual)
+
+    random1 = getRamdomIndex(individual)
+    random2 = getRamdomIndex(individual)
+
+    while random1 == random2:
+        random2 = getRamdomIndex(individual)
+        
+    temp = individual[random1[0]][random1[1]]
+    individual[random1[0]][random1[1]] = individual[random2[0]][random2[1]]
+    individual[random2[0]][random2[1]] = temp
+
+    fix_individual = [True, individual]
+    if( evaluate(individual,capacity,suppliers,current_capacity) == -1):
+        fix_individual = fix(individual,suppliers,capacity,current_capacity)
+    
+    if(fix_individual[0]):
+        return fix_individual
+    else:
+        temp = individual[random1[0]][random1[1]]
+        individual[random1[0]][random1[1]] = individual[random2[0]][random2[1]]
+        individual[random2[0]][random2[1]] = temp
+
+    return [False, individual]
+
+def crossover(individual1, individual2):
+    index1 = getRamdomIndex(individual1)
+    index2 = getRamdomIndex(individual2)
+
+    bit1 = individual1[index1[0]][index1[1]]
+    bit2 = individual2[index2[0]][index2[1]]
+
+    newIndividual1 = copy.deepcopy(individual1)
+    newIndividual2 = copy.deepcopy(individual2)
+    
+    for i1 in range(0, len(individual1)):
+        for i2 in range(0, len(individual1[i1])):
+            if (individual1[i1][i2] == bit1):
+                newIndividual1[i1][i2] = bit2
+            elif (individual1[i1][i2] == bit2):
+                newIndividual1[i1][i2] = bit1
+
+    for i1 in range(0, len(individual2)):
+        for i2 in range(0, len(individual2[i1])):
+            if (individual2[i1][i2] == bit1):
+                newIndividual2[i1][i2] = bit2
+            elif (individual2[i1][i2] == bit2):
+                newIndividual2[i1][i2] = bit1
+
+    if(newIndividual1 == individual1):
+        newIndividual1 = []
+    if(newIndividual2 == individual2):
+        newIndividual2 = []
+
+    return [newIndividual1,newIndividual2]
+
+
+############## SUBMAIN ###################
+
+def crossover_population(population, capacity, suppliers, n_cross, current_capacity = 0):
+
+    if current_capacity == 0:
+        current_capacity = capacity
+
+    count = 0
+    for _ in range(0, n_cross):
+        [individual1, individual2] = getRandomTwoIndividual(population)
+        [child1, child2] = crossover(individual1, individual2)
+
+        if (evaluate(child1, capacity, suppliers, current_capacity) == -1) and len(child1) != 0:
+            [can_fix, new_child] = fix(child1, suppliers, capacity, current_capacity)
+            if (can_fix == True):
+                count = count + 1
+                population.append(new_child)
+        elif len(child1) != 0:
+            count = count + 1
+            population.append(child1)
+
+        if (evaluate(child2, capacity, suppliers,current_capacity) == -1) and len(child2) != 0:
+            [can_fix, new_child] = fix(child2, suppliers, capacity, current_capacity)
+            if (can_fix == True):
+                count = count + 1
+                population.append(new_child)
+        elif len(child2) != 0:
+            count = count + 1
+            population.append(child2)
+
+    for individual in population:
+        for index,gen in  enumerate(individual):
+            if (len(gen)==0):
+                del individual[index]
+    return [count,population]
+
+def mutation_population(population, capacity, suppliers, n_muation,current_capacity):
+
+    count = 0
+    for _ in range(0, n_muation):
+        individual = getRandomIndividual(population)
+        result = mutation(individual, capacity, suppliers,current_capacity)
+        if (result[0]):
+            population.append(individual)
+            count = count+1
+    return [count, population]
+
+def enhance_population(population,capacity,suppliers, n_enhance,current_capacity):
+    count = 0
+
+    for _ in range(0,n_enhance):
+        [new_individual,index] = getRandomIndividual(population,True)
+        
+        for index,gen in  enumerate(new_individual):
+            if (len(gen)==0):
+                del new_individual[index]
+
+        [can_enhance,new_individual] = enhance(new_individual,capacity,suppliers,current_capacity)
+        
+        if(can_enhance and len(new_individual) !=0):
+            population.pop(index)
+        
+
+            population.append(new_individual)
+            count = count + 1
+    
+    return [count,population]
+
+def selection_population(population,n_selection):
+    population.sort(key=getScore)        #Sort là hàm mặc định từ nhỏ -> lớn, ở đây Sort số ngày
+   
+    population = population[:n_selection]
+
+    return population
+
+def GA(population,n_fix, capacity,suppliers, n_GA, n_cross, n_muation, n_enhance,n_selection, output ="", current_capacity = 0):
+
+    n_muation = int(n_muation * len(population))
+    n_enhance = int(n_enhance * len(population))
+    n_selection = int(n_selection * len(population))
+
+    Fi = population
+    count = 1
+    
+    if(output != ""):
+        f = open(output, "w")
+
+    for _ in range(n_GA):
+        
+
+        Fi = copy.deepcopy(Fi)
+
+        Fi = crossover_population(Fi, capacity, suppliers, n_cross,current_capacity)[1]
+
+        if(len(Fi) >=n_fix):
+            Fi= Fi[:n_fix]
+        
+        Fi = mutation_population(Fi,capacity,suppliers,n_muation,current_capacity)[1]
+       
+        if(len(Fi) >=n_fix):
+            Fi= Fi[:n_fix]
+       
+        Fi = enhance_population(Fi,capacity,suppliers,n_enhance,current_capacity)[1]
+        Fi = selection_population(Fi,n_selection)
+
+    
+        if(output != ""):
+            f.write('GENERATION '+str(count)+'\n')
+            for _ in Fi:
+                f.write(str(_)+ ' ===> Days required: '+ str(len(_))+'\n') 
+        else:
+            print('Đời F'+ str(count))
+            for _ in Fi:
+                print(str(_))
+
+        count = count +1
+        
+    return Fi
+
+######### MAIN ############
+
+[capacity,current_capacity,suppliers] = read_input("./input/input1.csv")
+
+F0 = makeF0(
+    suppliers = suppliers,
+    capacity = capacity,
+    current_capacity = current_capacity,
+    n_max=20
+)
+
+ga = GA(
+    population = F0,
+    capacity = capacity,
+    suppliers = suppliers,
+    n_fix = 50,
+    n_selection = 0.25,
+    n_GA = 100,
+    n_cross = 40,
+    n_muation = 0.25,
+    n_enhance = 0.5,
+    output = "output.txt",
+    current_capacity = current_capacity
+)
+
+write_output(ga,"./output/output.csv")

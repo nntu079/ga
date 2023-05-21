f4f2e6282bc63b4698bf4e93764caa77245dba4c
Tu <nntu079>
2023-05-21 23:04:13 +0700

    a

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import copy
 import random
 
-def crossover(individual1, individual2):
-
-    clone_individual1 = copy.deepcopy(individual1)
-    clone_individual2 = copy.deepcopy(individual2)
-
-   
+def crossover_helper(individual1, individual2,random_index1,random_index2):
     
+    clone_individual1 = copy.deepcopy(individual1)
+
     new_individual1 = []
     new_individual2 = []
 
@@ -22,54 +19,46 @@ def crossover(individual1, individual2):
     clone_new_individua1 = copy.deepcopy(new_individual1)
     clone_new_individua2 = copy.deepcopy(new_individual2)
 
-    len_individual = len(new_individual1)
-    random_index1 = random.randint(0,len_individual-1)
-    random_index2 = random.randint(random_index1,len_individual-1)
     
-
     bit_map1 = {}
 
-    for i in range(random_index1,random_index2 + 1):
-        if clone_new_individua1[i] not in bit_map1 and clone_new_individua2[i] not in bit_map1:
-            bit_map1[clone_new_individua1[i]]= clone_new_individua2[i]
-            bit_map1[clone_new_individua2[i]]= clone_new_individua1[i]
+    for i in range(random_index1,random_index2  + 1):
+        bit_map1[clone_new_individua2[i]]= clone_new_individua1[i]
 
     for index,value in enumerate(new_individual1):
-        if(value) in bit_map1:
-            new_individual1[index] = bit_map1[value]
-
-
-    bit_map2 = {}
-
-    for i in range(random_index1,random_index2 + 1):
-        if clone_new_individua1[i] not in bit_map2 and clone_new_individua2[i] not in bit_map2:
-            bit_map2[clone_new_individua1[i]]= clone_new_individua2[i]
-            bit_map2[clone_new_individua2[i]]= clone_new_individua1[i]
-
-    for index,value in enumerate(new_individual2):
-        if(value) in bit_map2:
-            new_individual2[index] = bit_map2[value]
+        if index >= random_index1 and index <= random_index2:
+            new_individual1[index] = new_individual2[index]
+        elif value in bit_map1:
+            x = bit_map1[value]
+            while(x in bit_map1):
+                x = bit_map1[x]
+            new_individual1[index] = x
 
     count = 0
     for index1,gen in enumerate(clone_individual1):
         for index2, bit in enumerate(gen):
             clone_individual1[index1][index2] = new_individual1[count]
             count +=1
+    
+    return clone_individual1
 
-    count = 0
-    for index1,gen in enumerate(clone_individual2):
-        for index2, bit in enumerate(gen):
-            clone_individual2[index1][index2] = new_individual2[count]
-            count +=1
+
+def crossover(individual1, individual2):
+
+    len_individual = len(individual1)
+    random_index1 = random.randint(0,len_individual-1)
+    random_index2 = random.randint(random_index1,len_individual-1)
 
     print(random_index1)
     print(random_index2)
-    return [clone_individual1, clone_individual2]
+
+    return [crossover_helper(individual1,individual2,random_index1,random_index2), 
+            crossover_helper(individual2,individual1,random_index1,random_index2)]
 
 
 
-individual1 = [[1,2,3],[4,5]]
-individual2 = [[1],[4],[5],[3,2]]
+individual1 = [[1,2,3],[4,5],[6]]
+individual2 = [[1],[4,6],[5],[3,2]]
 
 result = crossover(individual1,individual2)
 for individual in result:
